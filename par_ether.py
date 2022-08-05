d4d3b6eb771cf60904d2715fb70f9c38be63d559
evgsles <evgsles@yandex.ru>
2022-08-05 12:01:57 +0800

    bootstrap added

--- par_ether.py
+++ par_ether.py
@@ -1,15 +1,27 @@
-import json
 import time
 import datetime
 import requests
+import sqlite3
+import pandas as pd
+
 import config
 
 
 def get_histor_data():
+    try:
+        conn = sqlite3.connect("ether_data.db")
+        bd_data = pd.read_sql("SELECT * FROM ether_data", conn)
+        end_date = bd_data["date"][bd_data.last_valid_index()]
+        conn.close()
+    except:
+        end_date = "06-08-2015"
 
     url = "https://coingecko.p.rapidapi.com/coins/ethereum/history"
 
-    dates = [(datetime.date.today() - datetime.timedelta(days=x)).strftime('%d-%m-%Y') for x in range(0, 2762)]
+    start= datetime.datetime.strptime(datetime.date.today().strftime("%d-%m-%Y"), "%d-%m-%Y")
+    end = datetime.datetime.strptime(end_date, "%d-%m-%Y")
+
+    dates = [(datetime.date.today() - datetime.timedelta(days=x)).strftime('%d-%m-%Y') for x in range((start - end).days)][::-1]
     hist_info = []
     for date in dates:
         querystring = {"date":f"{date}","localization":"false"}
@@ -23,17 +35,21 @@ def get_histor_data():
         res_jsn = response.json()
         date_jsn = {"date":f"{date}"}
         hist_info.append({**date_jsn, **res_jsn})
-
     
-    with open('ether_data.json', 'a', encoding='utf-8') as f:
-        json.dump(hist_info, f, indent=4, ensure_ascii=False)
-
     return hist_info
 
 
-if __name__ == '__main__':
+def save_to_db(all_data):
+    df = pd.json_normalize(all_data)
+    conn = sqlite3.connect("ether_data.db")
+    df.to_sql("ether_data", conn, if_exists="append")
+    conn.close()
+
+
+if __name__ == "__main__":
     start = time.perf_counter()
-    get_histor_data()
+    all_data = get_histor_data()
+    save_to_db(all_data)
 
     fin = time.perf_counter() - start
     print(fin)
--- plotly_ether.py
+++ plotly_ether.py
@@ -1,130 +1,115 @@
-import json
+import sqlite3
 import pandas as pd
 import dash
+import dash_bootstrap_components as dbc
 from dash import dash_table, dcc, html
 from dash.dependencies import Input, Output
 
-with open("ether_data.json") as f:
-    all_data = json.load(f)
+conn = sqlite3.connect("ether_data.db")
+df = pd.read_sql('''SELECT date as 'Date', name as 'Name',
+             "market_data.current_price.usd" as 'Price USD',
+             "market_data.current_price.btc" as 'Price BTC',
+             "market_data.market_cap.usd" as 'Cap USD',
+             "market_data.market_cap.btc" as 'Cap BTC',
+             "market_data.market_cap.eth" as 'Cap ETH',
+             "market_data.total_volume.usd" as 'Volume USD',
+             "market_data.total_volume.btc" as 'Volume BTC',
+             "market_data.total_volume.eth" as 'Volume ETH',
+             "public_interest_stats.alexa_rank" as 'Alexa Rank'
+             FROM ether_data ORDER BY "index" DESC''', conn, index_col=None)
+conn.close()
 
-row_df = pd.json_normalize(all_data)
+df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y').dt.date
 
-df = row_df[["date", "name", "symbol",
-             "market_data.current_price.usd",
-             "market_data.current_price.btc",
-             "market_data.market_cap.usd",
-             "market_data.market_cap.btc",
-             "market_data.market_cap.eth",
-             "market_data.total_volume.usd",
-             "market_data.total_volume.btc",
-             "market_data.total_volume.eth",
-             "public_interest_stats.alexa_rank"
-             ]]
-df = df.rename(columns={"market_data.current_price.usd": "Price USD",
-                        "market_data.current_price.btc": "Price BTC",
-                        "market_data.market_cap.usd": "Cap USD",
-                        "market_data.market_cap.btc": "Cap BTC",
-                        "market_data.market_cap.eth": "Cap ETH",
-                        "market_data.total_volume.usd": "Volume USD",
-                        "market_data.total_volume.btc": "Volume BTC",
-                        "market_data.total_volume.eth": "Volume ETH",
-                        "public_interest_stats.alexa_rank": "Alexa Rank"
-                        })
-
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets = [dbc.themes.FLATLY])
 app.title = "ETH Historical Data"
 
-app.layout = html.Div([
-    dcc.Tabs(id="ether-graphs", value="tab-1-graph", children=[
-        dcc.Tab(label="ETH/USD", value="tab-1-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="ETH/BTC", value="tab-2-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Cap USD", value="tab-3-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Cap BTC", value="tab-4-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Cap ETH", value="tab-5-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Volume USD", value="tab-6-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Volume BTC", value="tab-7-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Volume ETH", value="tab-8-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-        dcc.Tab(label="Alexa Rank", value="tab-9-graph", 
-            selected_style={"padding": "5px"}, 
-            style={"padding": "5px"}),
-    ], style={"height": "36px"}),
-    html.Div(id="ether-datatable-container", style={"margin-top": "20px"}),
-    dash_table.DataTable(
+app.layout = dbc.Container([
+    dbc.Tabs(id="ether-graphs", active_tab="tab-1-graph", children=[
+        dbc.Tab(label="ETH/USD", tab_id="tab-1-graph"),
+        dbc.Tab(label="ETH/BTC", tab_id="tab-2-graph"),
+        dbc.Tab(label="Cap USD", tab_id="tab-3-graph"),
+        dbc.Tab(label="Cap BTC", tab_id="tab-4-graph"),
+        dbc.Tab(label="Cap ETH", tab_id="tab-5-graph"),
+        dbc.Tab(label="Volume USD", tab_id="tab-6-graph"),
+        dbc.Tab(label="Volume BTC", tab_id="tab-7-graph"),
+        dbc.Tab(label="Volume ETH", tab_id="tab-8-graph"),
+        dbc.Tab(label="Alexa Rank", tab_id="tab-9-graph"),
+    ]),
+    dbc.Row(id="ether-datatable-container", style={"margin-top": "10px"}),
+    dbc.Row(dash_table.DataTable(
         id="ether-datatable",
         columns=[
-            {"name": i, "id": i, "deletable": False, "selectable": False} for i in df.columns
+            {"name": i, "id": i} for i in df.columns
         ],
         data=df.to_dict("records"),
+        fixed_rows = {"headers":True},
         filter_action="native",
         sort_action="native",
         sort_mode="multi",
-        selected_columns=[],
         selected_rows=[],
         page_action="native",
         page_current=0,
         page_size=10,
         style_table={"overflowX": "auto"},
-        style_cell={"textAlign": "left"},
-        style_header={"backgroundColor": "#8e8e8e", "color": "white", "textAlign": "center"},
-        style_data={"backgroundColor": "#f0f0f0",},
-        style_filter={"backgroundColor": "#d5d5d5"}
-    )
-], style={"margin-left": "80px", "margin-right": "80px"})
-
-
-@app.callback(
-    Output("ether-datatable", "style_data_conditional"),
-    Input("ether-datatable", "selected_columns"))
-def update_styles(selected_columns):
-    return [{"column_id": i} for i in selected_columns]
-
+        style_cell = {
+            "textAlign":"center",
+            "fontFamily":"Arial",
+            "border":"4px solid white",
+            "maxWidth" :"50px",
+            "textOverflow": "ellipsis",
+        },
+        style_header = {
+            "color": "white",
+            "backgroundColor":"rgb(125,125,125)",
+            "fontWeight":"bold",
+            "border":"4px solid white",
+            "fontSize":"12px"
+        },
+        style_data_conditional = [
+            {
+                "if": {"row_index": "odd"},
+                "backgroundColor": "rgb(240,240,240)",
+            },            
+            {
+                "if": {"row_index": "even"},
+                "backgroundColor": "rgb(255, 255, 255)",     
+            }
+        ],
+        style_data={"fontSize":"12px"},
+        style_filter={"backgroundColor": "rgb(220,220,220)"}
+    ))
+])
 
 @app.callback(Output("ether-datatable-container", "children"),
-              Input("ether-graphs", "value"),
+              Input("ether-graphs", "active_tab"),
               Input("ether-datatable", "derived_virtual_data"))
 def update_graphs(tab, rows):
     dff = df if rows is None else pd.DataFrame(rows)
     graphs = [
-        html.Div(dcc.Graph(
+        dbc.Row(dcc.Graph(
             id=column,
             figure={
                 "data": [
                     {
-                        "x": dff["date"],
+                        "x": dff["Date"],
                         "y": dff[column],
                         "type": "line",
-                        "marker": {"color": "lightblue"},
+                        "marker": {"color": "rgb(108,195,175)"},
                     }
                 ],
                 "layout": {
-                    "xaxis": {"automargin": True,
-                              "autorange": "reversed"},
+                    "xaxis": {"automargin": True},
                     "yaxis": {
                         "automargin": True,
                         "title": {"text": column}
                     },
                     "height": 400,
                     "margin": {"t": 10, "l": 0, "r": 0},
-                    "padding": {"t": 0, "l": 5, "r": 5},
+                    "padding": {"t": 0, "l": 3, "r": 3},
                 },
             },
-        ), style={"margin-top": "20px", "margin-bottom": "20px"})
+        ), style={"margin-top": "10px", "margin-bottom": "10px"})
 
         for column in ["Price USD",
                        "Price BTC",
@@ -138,47 +123,47 @@ def update_graphs(tab, rows):
                        ] if column in dff
     ]
     if tab == "tab-1-graph":
-        return html.Div([
-            html.H3("ETH/USD"),
+        return dbc.Row([
+            html.H4("ETH/USD"),
             graphs[0]
         ])
     elif tab == "tab-2-graph":
-        return html.Div([
-            html.H3("ETH/BTC"),
+        return dbc.Row([
+            html.H4("ETH/BTC"),
             graphs[1]
         ])
     elif tab == "tab-3-graph":
-        return html.Div([
-            html.H3("Cap USD"),
+        return dbc.Row([
+            html.H4("Cap USD"),
             graphs[2]
         ])
     elif tab == "tab-4-graph":
-        return html.Div([
-            html.H3("Cap BTC"),
+        return dbc.Row([
+            html.H4("Cap BTC"),
             graphs[3]
         ])
     elif tab == "tab-5-graph":
-        return html.Div([
-            html.H3("Cap ETH"),
+        return dbc.Row([
+            html.H4("Cap ETH"),
             graphs[4]
         ])
     elif tab == "tab-6-graph":
-        return html.Div([
-            html.H3("Volume USD"),
+        return dbc.Row([
+            html.H4("Volume USD"),
             graphs[5]
         ])
     elif tab == "tab-7-graph":
-        return html.Div([
-            html.H3("Volume BTC"),
+        return dbc.Row([
+            html.H4("Volume BTC"),
             graphs[6]
         ])
     elif tab == "tab-8-graph":
-        return html.Div([
-            html.H3("Volume ETH"),
+        return dbc.Row([
+            html.H4("Volume ETH"),
             graphs[7]
         ])
     elif tab == "tab-9-graph":
-        return html.Div([
+        return dbc.Row([
             html.H3("Alexa Rank"),
             graphs[-1]
         ])
